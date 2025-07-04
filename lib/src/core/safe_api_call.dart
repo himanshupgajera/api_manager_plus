35ecb187d35f7cab4ca20c0191f6e31e251340b4
Himanshu <divine.developer2024@gmail.com>
2025-07-04 15:28:00 +0530

    update connectivity plus package

--- lib/src/core/safe_api_call.dart
+++ lib/src/core/safe_api_call.dart
@@ -2,9 +2,16 @@ import 'package:connectivity_plus/connectivity_plus.dart';
 
 class SafeApiCall {
   static Future<T> call<T>(Future<T> Function() apiFunction) async {
-    final connectivity = await Connectivity().checkConnectivity();
-    if (connectivity == ConnectivityResult.none) {
-      throw 'No Internet connection. Please check your connection and try again.';
+    final List<ConnectivityResult> connectivity =
+        await Connectivity().checkConnectivity();
+
+    final bool hasInternet = connectivity.contains(ConnectivityResult.mobile) ||
+        connectivity.contains(ConnectivityResult.wifi) ||
+        connectivity.contains(ConnectivityResult.ethernet) ||
+        connectivity.contains(ConnectivityResult.vpn);
+
+    if (!hasInternet) {
+      throw 'No internet connection. Please check your connection and try again.';
     }
 
     try {
